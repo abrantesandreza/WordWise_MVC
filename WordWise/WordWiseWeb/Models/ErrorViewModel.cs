30c62ec50f9f0b5e8183532b7ef86c75e2b94944
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-06-19 22:07:44 -0300

    Initial commit

---
+++ WordWise/WordWiseWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WordWiseWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
