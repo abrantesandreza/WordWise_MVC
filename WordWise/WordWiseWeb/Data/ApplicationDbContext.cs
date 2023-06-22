848b71a11767f1ed608a80720eeefddd9a2ce520
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-06-22 16:48:54 -0300

    Migration done

--- WordWise/WordWiseWeb/Data/ApplicationDbContext.cs
+++ WordWise/WordWiseWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WordWiseWeb.Models;
 
 namespace WordWiseWeb.Data
 {
@@ -8,5 +9,7 @@ namespace WordWiseWeb.Data
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
