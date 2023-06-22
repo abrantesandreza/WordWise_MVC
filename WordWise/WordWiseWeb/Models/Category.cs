a016994bd1440aa4d451c585c1f5f918ec75d9f7
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-06-21 23:27:01 -0300

    Configs iniciais: Create Category Model, Data Annotations, Nuget Packages, Connection String and ApplicationDbContext setup

---
+++ WordWise/WordWiseWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace WordWiseWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
