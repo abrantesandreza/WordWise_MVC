f6ff75b77dc6bb43a1aec78b17e90b87b93bcbe1
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-07-03 15:49:10 -0300

    Get all categories e hot reload

--- WordWise/WordWiseWeb/Controllers/CategoryController.cs
+++ WordWise/WordWiseWeb/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using WordWiseWeb.Data;
+using WordWiseWeb.Models;
 
 namespace WordWiseWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
