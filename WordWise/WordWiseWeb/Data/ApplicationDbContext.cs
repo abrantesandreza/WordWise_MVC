7a50dc4f1f7a4849b030516a483f8fe82e221a3a
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-07-03 15:37:18 -0300

    Seed Category table

--- WordWise/WordWiseWeb/Data/ApplicationDbContext.cs
+++ WordWise/WordWiseWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,17 @@ namespace WordWiseWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Romance", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Law", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Self-Help & Relationships", DisplayOrder = 5 },
+                new Category { Id = 6, Name = "Science & Technology", DisplayOrder = 6 }
+                );
+        }
     }
 }
